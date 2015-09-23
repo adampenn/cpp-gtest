8135e16d44643e7c2b021ab2508c3da868be164c
Adam J. Penn <madannep@gmail.com>
2015-09-23 08:32:32 -0700

    added test to randoTest.cpp that test the childSmiles Function.

--- randoTest.cpp
+++ randoTest.cpp
@@ -5,7 +5,7 @@
 #include <gtest/gtest.h>
 #include "rando.h"
 
-/*  
+/*
 	FUNCTIONS WE NEED TO TEST:
 
 	bool shouldWorry(bool,bool,bool);
@@ -13,18 +13,42 @@
 	bool isPrime(int);
 	int nearestToZero(int,int);
 */
- 
+
 class RandoTest : public ::testing::Test
 {
 	protected:
 		RandoTest(){} //constructor runs before each test
 		virtual ~RandoTest(){} //destructor cleans up after tests
 		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor) 
+		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
 TEST(RandoTest, allChildrenSmile)
 {
 	Rando rando;
-	ASSERT_TRUE( rando.shouldWorry(true,true,true) );
+  ASSERT_TRUE( rando.shouldWorry(true,true,true) );
+}
+
+TEST(RandoTest, fistChildSmiles)
+{
+	Rando rando;
+  ASSERT_TRUE( rando.shouldWorry(true,false,false) );
+}
+
+TEST(RandoTest, secondChildSmiles)
+{
+	Rando rando;
+  ASSERT_TRUE( rando.shouldWorry(false,true,false) );
+}
+
+TEST(RandoTest, thirdChildSmiles)
+{
+	Rando rando;
+  ASSERT_TRUE( rando.shouldWorry(false,false,true) );
+}
+
+TEST(RandoTest, twoChildrenSmile)
+{
+	Rando rando;
+  ASSERT_FALSE( rando.shouldWorry(false,true,true) );
 }
